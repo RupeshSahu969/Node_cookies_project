687f48e7cf211bd59f202a6295c4db632ae54be8
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-07-03 12:03:41 +0530

    change this cookies

--- Server.js
+++ Server.js
@@ -17,23 +17,7 @@ app.get('/get-cookie', (req, res) => {
 
 const { body, validationResult } = require('express-validator');
 
-app.post('/register',
-  // Validation rules
-  [
-    body('username').isLength({ min: 5 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 6 })
-  ],
-  // Handle validation results
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    // Proceed with registration
-    res.send('User registered successfully');
-  }
-);
+
   
 
 app.listen(3000, () => {
